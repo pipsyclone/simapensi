b04a03c8940f1b06faf89c5c41b635acf7ca4723
unknown <apipska.10@gmail.com>
2024-12-10 18:45:40 +0700

    Update Version

--- app/api/absence/post/schedule/route.js
+++ app/api/absence/post/schedule/route.js
@@ -3,6 +3,17 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
 	try {
+		// Periksa apakah hari ini adalah Minggu
+		const today = new Date();
+		const dayOfWeek = today.getDay(); // 0 = Minggu, 1 = Senin, ..., 6 = Sabtu
+
+		if (dayOfWeek === 0) {
+			return NextResponse.json({
+				status: 400,
+				message: "Hari minggu tidak ada update absensi!",
+			});
+		}
+
 		const datas = await prisma.user.findMany({
 			where: { role: { contains: "EMPLOYEE" } },
 			select: {
